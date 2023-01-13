b654f8db6955e4e89214319f44bd11c636fa766a
Sleptsov Oleg <sleptsov.o@bk.ru>
2023-01-13 16:02:03 +0700

    Добавлена кнопка при нажатии на которую на табло отображается количество нажатий

--- Sprint3/ViewController.swift
+++ Sprint3/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var Button: UIButton!
+    
+    private var score : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        score = 0
+        Label.text = "Значение счетчика: \(score)"
     }
 
-
+    @IBAction func ButtonTap(_ sender: Any) {
+        score = score + 1
+        Label.text = "Значение счетчика: \(score)"
+    }
+    
 }
 
